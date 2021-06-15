5e6c0d1da5bd5adfcc0cb973fede9551f17d974b
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2021-06-15 08:45:10 -0300

    dev

---
+++ resources/views/layouts/horizontal-menu.blade.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="{{ asset('css/stroke.css') }}">
+
+<nav class="stroke mb-5">
+    <ul>
+      <li><a href=""><i class="fas fa-tachometer-alt fa-lg mr-1 "></i></i>Dashboard</a></li>
+      <li><a href="">Devedores</a></li>
+      <li><a href="">Financeiro</a></li>
+      <li><a href="">More</a></li>
+      <li><a href="">Nice staff</a></li>
+    </ul>
+  </nav>
+</div>
